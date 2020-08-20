7b23d46c71e5aa23ce596ffba4f8220b5c1a3601
Segbedzi Edem <segbedzi.edem@gmail.com>
2020-08-20 15:13:08 +0000

    changes for public repo

--- buildAndRun.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-git pull origin master;
-mvn clean package;
-fuser -k 9095/tcp;
-nohup java -jar target/mindITCovid19.jar 1>/dev/null 2>/dev/null &
--- deployment.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-ssh javatar@144.91.101.249 "cd /home/javatar/apps/mindit/mindit-covid19-rancard; git pull origin master;fuser -k 9095/tcp;mvn clean package; nohup java -jar target/mindITCovid19.jar 1>/dev/null 2>/dev/null & exit"
